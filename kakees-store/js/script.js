856cf41b3e5ff4c630c2148dfa4eafabecbda68c
Leviosa555 <114020469+Leviosa555@users.noreply.github.com>
2024-12-11 06:31:21 -0500

    Delete kakees-store directory

--- kakees-store/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.hover-effect').hover(function() {
-        $(this).css('transform', 'scale(1.1)');
-    }, function() {
-        $(this).css('transform', 'scale(1)');
-    });
-});
